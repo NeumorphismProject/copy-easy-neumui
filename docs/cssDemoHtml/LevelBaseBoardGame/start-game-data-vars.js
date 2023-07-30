ed632a8151ac2e187a2860727be20ee2db8ca313
MicrolAsusPc <414464322@qq.com>
2023-07-30 12:06:08 +0800

    docs: board game channel

--- docs/cssDemoHtml/LevelBaseBoardGame/start-game-data-vars.js
+++ docs/cssDemoHtml/LevelBaseBoardGame/start-game-data-vars.js
@@ -1,12 +1,12 @@
 /** 游戏关键数据的全局变量脚本：该文件会被作为游戏数据模板处理，sence-creator.html 中会读取这些模板数据，然后进行打包生成一份新的配置数据 */
 
-const GAME_SENCE_BACKGROUND_IMAGE = '' // 整个游戏场景的背景图（为了游戏效果，背景图不会自适应，游戏场景中的每个物体目前均以px为单位进行布局，位置需要与背景图相对应）
-const PLAYER_X_START_SPEED = 4 // 玩家移动初速度
-const PLAYER_Y_FREE_FALL_STEP_SPEED = 8 // 控制玩家下落速度的单位值
-const PLAYER_Y_JUMP_START_SPEED = 20 // 玩家跳跃的初速度值
-const PLAYER_BORTH_POSITION = [250, 0] // 玩家出生位置
-const COIN_COLLISION_OFFSET = 10 // 玩家与金币碰撞的边缘计算偏移量（为了体验更好，玩家需要进入金币div一定的范围内部才会让金币被碰撞而消失）
-const PLAYER_SPRITES_BACKGROUND_POSITION_DEFALUT = [5.5, 7] // 玩家雪碧图背景的 background-position 的初始值（实际在css中的单位时百分比%）
+let GAME_SENCE_BACKGROUND_IMAGE = '' // 整个游戏场景的背景图（为了游戏效果，背景图不会自适应，游戏场景中的每个物体目前均以px为单位进行布局，位置需要与背景图相对应）
+let PLAYER_X_START_SPEED = 4 // 玩家移动初速度
+let PLAYER_Y_FREE_FALL_STEP_SPEED = 8 // 控制玩家下落速度的单位值
+let PLAYER_Y_JUMP_START_SPEED = 20 // 玩家跳跃的初速度值
+let PLAYER_BORTH_POSITION = [250, 0] // 玩家出生位置
+let COIN_COLLISION_OFFSET = 10 // 玩家与金币碰撞的边缘计算偏移量（为了体验更好，玩家需要进入金币div一定的范围内部才会让金币被碰撞而消失）
+let PLAYER_SPRITES_BACKGROUND_POSITION_DEFALUT = [5.5, 7] // 玩家雪碧图背景的 background-position 的初始值（实际在css中的单位时百分比%）
 
 // 玩家运动初速度 [x,y]
 let playerSpeed = [0, 0]
@@ -22,10 +22,10 @@ let playerSpritesRunActionAnimationOptions = {
 }
 
 // 游戏画面中的砖块集合
-let blocksData = []
+let blocksData = undefined // [] 使用undefined是为了游戏创建场景物体时进行识别使用demo数据还是配置数据
 // 金币集合
 let coinsData = {
-  value: [],
+  value: undefined, // [] 使用undefined是为了游戏创建场景物体时进行识别使用demo数据还是配置数据
   removeCoinId: null // 通过对 coinsDataProxy.removeDomId = 1; 这种方式传值可以实现删除 value 中指定的数组元素，且会自动移除对应的dom元素
 }
 
