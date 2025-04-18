5d7f3df49f089c1e84c3c8702ad9044b3a18533a
v-kunlunma <v-kunlunma@microsoft.com>
2025-04-18 10:14:12 +0800

    feat: nextconfig try public web

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  // -- 用于借助 Next.js 部署后的服务来同时部署vue项目的打包文件 （的重定向功能，将 /vue-app/ 下的所有路径都重定向到 Vue 的 index.html 文件）
+  async rewrites() {
+    return [
+      {
+        source: '/web/:path*', // 匹配 /vue-app/ 下的所有路径
+        destination: '/web/index.html', // 重定向到 Vue 的 index.html
+      },
+    ]
+  },
+  // --
   swcMinify: true,
   compiler: {
     styledComponents: true,
